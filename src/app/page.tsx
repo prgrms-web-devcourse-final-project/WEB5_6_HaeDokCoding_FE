eb322fe159fa447ac7dc50ab867c785edf98637a
mtm1018 <mtm1018@naver.com>
2025-09-21 21:54:35 +0900

    chore/dev브랜치 추가 및 폰트스타일 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@
 export default function Home() {
 
   return (
-    <div className="text-tertiary font-serif flex-center">
-      DEV 브랜치 만들것 
+    <div className="flex-center">
+      <h1> 이게 기본</h1>
+      <h1 className="font-serif">this is Serif</h1>
     </div>
   )
 }
