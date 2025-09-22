e85d2862bab773c93062414b24d8dbc5963c0ada
mtm1018 <mtm1018@naver.com>
2025-09-22 22:26:27 +0900

    [test]svg 테스트

--- next.config.ts
+++ next.config.ts
@@ -1,45 +1,27 @@
-import type { NextConfig } from 'next';
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // TurboPack 설정
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
+  // output: 'export',
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+  turbopack: {
+    rules: {
+      '.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '.js',
       },
     },
-  },
-  // webpack 설정
-  webpack: (config) => {
-    // @ts-expect-error 타입 에러 무시
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              typescript: true,
-              ext: 'tsx',
-            },
-          },
-        ],
-      }
-    );
-    fileLoaderRule.exclude = /\.svg$/i;
-    return config;
   },
 };
 
