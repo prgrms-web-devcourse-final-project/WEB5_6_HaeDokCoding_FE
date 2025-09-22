c30f92a7f1b21cb1c6ad9ecb4aee9ea09c2b195e
EunbinJung <ainatube39@gmail.com>
2025-09-22 22:17:31 +0900

    [fix] next.config파일 수정

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,27 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
+  // output: 'export',
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
       },
     },
   },
