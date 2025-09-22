9050cae4d772dada0809ed7412b7214f8dd7e7cb
EunbinJung <ainatube39@gmail.com>
2025-09-22 10:49:46 +0900

    [test]test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="flex-center">
       <h1> 이게 기본</h1>
       <h1 className="font-serif">this is Serif</h1>
+      <h1>테스트입니다</h1>
     </div>
   );
 }
