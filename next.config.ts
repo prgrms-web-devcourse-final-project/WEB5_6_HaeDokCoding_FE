7d740352d4efe080cc53a65369c8176b063cfa50
mtm1018 <mtm1018@naver.com>
2025-09-23 00:55:48 +0900

    [style] selectbox

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,45 @@
-import { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // output: 'export',
-  reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
-  compiler: {
-    // ssr and displayName are configured by default
-    styledComponents: true,
-  },
+  // TurboPack 설정
   experimental: {
     turbo: {
       rules: {
-        '.svg': {
+        '*.svg': {
           loaders: ['@svgr/webpack'],
-          as: '.js',
+          as: '*.js',
         },
       },
     },
+  },
+  // webpack 설정
+  webpack: (config) => {
+    // @ts-expect-error 타입 에러 무시
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              typescript: true,
+              ext: 'tsx',
+            },
+          },
+        ],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
   },
 };
 
