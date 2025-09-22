52be315724876db4a9be89db95788c6cbba2ffe7
mtm1018 <mtm1018@naver.com>
2025-09-22 22:26:27 +0900

    [test]svg 테스트

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,28 @@
-import { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  // output: 'export',
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
   turbopack: {
     rules: {
-      '*.svg': {
+      '.svg': {
         loaders: ['@svgr/webpack'],
-        as: '*.js',
+        as: '.js',
       },
-    }
+    },
   },
-}
+};
 
 export default nextConfig;
