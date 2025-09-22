0a7b6370603cb3021815b24f34fa590bcdda6f22
mtm1018 <mtm1018@naver.com>
2025-09-23 00:55:48 +0900

    [style] selectbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import Edit from '@/shared/assets/icons/edit_28.svg'
-
-
 export default function Home() {
   return (
     <div className="flex-center">
       <h1> 이게 기본</h1>
       <h1 className="font-serif">this is Serif</h1>
-      <Edit></Edit>
       <h1>테스트입니다</h1>
     </div>
   );
--- src/shared/components/InputBox/SelectBox.tsx
+++ src/shared/components/InputBox/SelectBox.tsx
@@ -1,17 +1,37 @@
-import { Ref } from "react";
+import { Ref, useState } from "react";
+import Down from '@/shared/assets/icons/selectDown_24.svg'
 
 interface Props{
   id: string
-  ref?: Ref<HTMLSelectElement | null>
+  ref?: Ref<HTMLLabelElement | null>
+  option: string[]
+  title:string
 }
 
-function SelectBox({id,ref}:Props) {
+function SelectBox({ id, ref, option,title }: Props) {
+  
+const [isOpen,setIsOpen] = useState(false)
+const [select,setSelect] = useState('')
   return (
-    <>
-      <label htmlFor={id}>selectBox</label>
-
-      <select id={id} ref={ref}></select>
-    </>
+    <div className="flex flex-col gap-2 relative h-6">
+      <label
+        htmlFor={id}
+        ref={ref}
+        className="flex gap-2 cursor-pointer text-base"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+       {select ? select : title}<Down />
+      </label>
+      {isOpen && (
+        <ul className="w-30 bg-white text-gray-dark p-2 rounded-xl absolute top-8">
+          {option.map((v, i) => (
+            <li key={v + i} className="cursor-pointer p-1 hover:bg-secondary" onClick={() => { setSelect(v), setIsOpen(!isOpen) }}>
+              {v}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
 export default SelectBox
