d905e7e7a333f9d8ba8aa4c137b1e3a8ac012eff
mtm1018 <mtm1018@naver.com>
2025-09-23 09:47:48 +0900

    [style] 인풋 수정사항

--- src/shared/components/InputBox/Input.tsx
+++ src/shared/components/InputBox/Input.tsx
@@ -1,9 +1,15 @@
 import tw from '@/shared/utills/tw';
 import { cva } from 'class-variance-authority';
-import { Ref } from 'react';
+import { HTMLInputTypeAttribute, Ref } from 'react';
+
+// select나올떄 자연스러운 처리 화살표 로테이트 
+// 인풋 타입받을 수 있게 수정
+// 인풋접근성 라벨이 중요함 라벨 을 div에 묶어서 하거나 label로 인풋감싸거나 div로 묶고 같은 선상에두게 
+// div안에 라벨이랑감싸기
 
 interface Props {
   placeholder: string;
+  type?: HTMLInputTypeAttribute
   ref?: Ref<HTMLInputElement | null>;
   size?: 'default' | 'lg';
   className?: string;
--- src/shared/components/InputBox/SelectBox.tsx
+++ src/shared/components/InputBox/SelectBox.tsx
@@ -31,7 +31,8 @@ function SelectBox({ id, ref, option, title }: Props) {
       >
         {select ? select : title}
         <Down />
-      </label>
+      </label> 
+      {/* 버튼으로 바뀌어서 아리d아 익스펜디드 */}
       {isOpen && (
         <ul className="w-30 bg-white text-gray-dark p-2 rounded-xl absolute top-8">
           {option.map((v, i) => (
@@ -43,7 +44,9 @@ function SelectBox({ id, ref, option, title }: Props) {
               {v || title}
             </li>
           ))}
-        </ul>
+        </ul> 
+
+        // 아리아 셀렉티드 눌러서 스크린에서 인지할수있게 role ul role:리스트박스 li: option  아리아 셀렉티드
       )}
     </div>
   );
