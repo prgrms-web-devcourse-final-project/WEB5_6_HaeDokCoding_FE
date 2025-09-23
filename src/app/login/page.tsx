e2e715bce90285672fc8a13d8399c7b826515410
ahk0413 <rladkgusgd@gmail.com>
2025-09-23 16:41:22 +0900

    [design] 로그인 페이지 ui 구현

---
+++ src/app/login/page.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import type { Metadata } from 'next';
+import SocialLoginButtons from './SocialLoginButtons';
+import loginBg from '@/shared/assets/images/login_bg.webp';
+
+export const metadata: Metadata = {
+  title: 'SSOUL | 로그인',
+  description: '칵테일을 좋아하는 사람들을 위한 서비스 로그인 페이지',
+};
+
+function Page() {
+  return (
+    <div className="relative flex flex-1 flex-col items-center justify-center gap-4 min-h-screen">
+      <div
+        className="absolute bottom-0 left-1/2 -translate-x-1/2 w-full max-w-[75rem] h-full -z-10"
+        aria-hidden
+      >
+        <Image src={loginBg} alt="" fill className="object-cover md:object-contain object-bottom" />
+      </div>
+      <div className="flex flex-col gap-3 text-center">
+        <h1 className="text-4xl font-bold">로그인</h1>
+        <p>3초 로그인으로 SSOUL을 가볍게 즐겨보세요🍸</p>
+      </div>
+      <SocialLoginButtons />
+    </div>
+  );
+}
+export default Page;
