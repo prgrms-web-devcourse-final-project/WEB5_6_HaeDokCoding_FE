b793afbf02c2726283934da836c4502566d38426
EunbinJung <ainatube39@gmail.com>
2025-09-23 10:47:34 +0900

    [chore] 깃허브 링크 추가, 오타 수정

--- src/components/footer/Github.tsx
+++ src/components/footer/Github.tsx
@@ -4,7 +4,11 @@ import Icon from '@/shared/assets/icons/github_32.svg';
 function Github() {
   return (
     <div className="cursor-pointer hover:opacity-80 transition-opacity">
-      <Link href="">
+      <Link
+        href="https://github.com/prgrms-web-devcourse-final-project/WEB5_6_HaeDokCoding_FE"
+        target="_blank"
+        aria-label="깃허브 링크"
+      >
         <Icon />
       </Link>
     </div>
