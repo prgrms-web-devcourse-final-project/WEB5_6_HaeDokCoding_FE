f3f5510bb9cfa88d1156cdc194ed28d064629f61
mtm-git1018 <mtm1018@naver.com>
2025-09-23 10:02:55 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,46 @@
+import type { NextConfig } from 'next';
 
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config: { module: { rules: { test: RegExp; use: string[]; }[]; }; }) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+const nextConfig: NextConfig = {
+  // TurboPack 설정
+  experimental: {
+    turbo: {
+      rules: {
+        '.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '.js',
+        },
+      },
+    },
+  },
+  // webpack 설정
+  webpack: (config) => {
+    // @ts-expect-error 타입 에러 무시
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
 
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              typescript: true,
+              ext: 'tsx',
+            },
+          },
+        ],
+      }
+    );
+    fileLoaderRule.exclude = /.svg$/i;
     return config;
   },
 };
-
-module.exports = nextConfig;
 
 export default nextConfig;
