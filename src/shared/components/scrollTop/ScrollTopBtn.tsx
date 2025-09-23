ed00f0b8cf477c04cfecee011463502ede285130
ahk0413 <rladkgusgd@gmail.com>
2025-09-23 15:02:49 +0900

    [fix] scroll 없을시 안보이게 로직 추가

--- src/shared/components/scrollTop/ScrollTopBtn.tsx
+++ src/shared/components/scrollTop/ScrollTopBtn.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import Arrow from '@/shared/assets/icons/arrow_up_24.svg';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function ScrollTopBtn() {
   const animationRef = useRef<number | null>(null);
+  const [isVisible, setIsVisible] = useState(false);
 
   // scrollTop 버튼 클릭 시
   const scrollToTop = () => {
@@ -24,16 +25,23 @@ function ScrollTopBtn() {
       }
     };
 
+    const handleScroll = () => {
+      const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      setIsVisible(currentScroll > 30);
+    };
+
+    window.addEventListener('scroll', handleScroll, { passive: true });
     window.addEventListener('wheel', cancelScroll, { passive: true });
     window.addEventListener('touchstart', cancelScroll, { passive: true });
 
     return () => {
+      window.removeEventListener('scroll', handleScroll);
       window.removeEventListener('wheel', cancelScroll);
       window.removeEventListener('touchstart', cancelScroll);
     };
   }, []);
 
-  // if (!isVisible) return null;
+  if (!isVisible) return null;
   return (
     <div className="fixed right-6 bottom-6 z-50">
       <button
