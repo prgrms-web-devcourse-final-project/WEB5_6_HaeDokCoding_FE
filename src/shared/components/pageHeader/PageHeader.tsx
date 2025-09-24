7de06fc99c421ece6367e139b64514d8cf1c1db0
mtm1018 <mtm1018@naver.com>
2025-09-24 14:22:19 +0900

    [style]page header컴포넌트

--- src/shared/components/pageHeader/PageHeader.tsx
+++ src/shared/components/pageHeader/PageHeader.tsx
@@ -10,16 +10,20 @@ interface Props {
 function PageHeader({ src, title, description }: Props) {
   return (
     <div
-      className="h-100 w-full bg-repeat relative"
+      className="h-40 w-full bg-repeat o-cover relative md:h-70 lg:h-100"
       style={{
         backgroundImage: `url(${Star.src})`,
       }}
     >
       <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
-        <Image src={src} alt="" />
-        <div className="text-center text-nowrap font-serif font-bold flex flex-col absolute bottom-[55px]">
-          <h2 className="text-[64px]">{title}</h2>
-          <p className="text-2xl">{description}</p>
+        <Image src={src} alt="" width={src.width} height={src.height} />
+        <div className="text-center text-nowrap font-serif font-bold flex flex-col absolute left-1/2 -translate-x-1/2 bottom-4 md:bottom-9 lg:bottom-[55px]">
+          <h2 className="text-shadow-[0_0_12px_#1a1a1a]  text-2xl md:text-5xl lg:text-3xl lg:text-[64px]">
+            {title}
+          </h2>
+          <p className="text-shadow-[0_0_12px_#1a1a1a] text-xs md:text-xl lg:text-2xl">
+            {description}
+          </p>
         </div>
       </div>
     </div>
