7af287c7a4d18bbb62231562d454eac8a1626002
EunbinJung <ainatube39@gmail.com>
2025-09-24 22:19:27 +0900

    [fix] 레이아웃 수정

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,8 +1,9 @@
 import CommunityFilter from '@/shared/components/community/CommunityFilter';
-import CommunityHeader from '@/shared/components/community/CommunityHeader';
 import CommunityTab from '@/shared/components/community/CommunityTab';
 import PostCard from '@/shared/components/community/PostCard';
 import WriteBtn from '@/shared/components/community/WriteBtn';
+import PageHeader from '@/shared/components/pageHeader/PageHeader';
+import headerImg from '@/shared/assets/images/community_page_header.webp';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -12,30 +13,32 @@ export const metadata: Metadata = {
 
 function Page() {
   return (
-    <div className="page-layout max-w-1024">
-      <div className="mt-3 mb-10 flex flex-col gap-8 ">
-        <section aria-labelledby="community-heading">
-          <h1 id="community-heading" className="sr-only">
-            커뮤니티 페이지
-          </h1>
-          <CommunityHeader />
-        </section>
+    <div className="w-full">
+      <PageHeader src={headerImg} title="Community" description="칵테일에 관한 모든 이야기" />
+      <div className="page-layout max-w-1024">
+        <div className="mt-3 mb-10 flex flex-col gap-8 ">
+          <section aria-labelledby="community-heading">
+            <h1 id="community-heading" className="sr-only">
+              커뮤니티 페이지
+            </h1>
+          </section>
 
-        <section
-          aria-label="탭과 글쓰기"
-          className="flex justify-between item-center sm:flex-row flex-col gap-4 mt-1"
-        >
-          <CommunityTab />
-          <WriteBtn />
-        </section>
+          <section
+            aria-label="탭과 글쓰기"
+            className="flex justify-between item-center sm:flex-row flex-col gap-4 mt-1"
+          >
+            <CommunityTab />
+            <WriteBtn />
+          </section>
 
-        <section aria-label="게시물 목록">
-          <CommunityFilter />
-          <PostCard label="레시피" />
-          <PostCard label="팁" />
-          <PostCard label="질문" />
-          <PostCard label="자유" />
-        </section>
+          <section aria-label="게시물 목록">
+            <CommunityFilter />
+            <PostCard label="레시피" />
+            <PostCard label="팁" />
+            <PostCard label="질문" />
+            <PostCard label="자유" />
+          </section>
+        </div>
       </div>
     </div>
   );
--- src/shared/components/header/HeaderBtn.tsx
+++ src/shared/components/header/HeaderBtn.tsx
@@ -14,16 +14,13 @@ function HeaderBtn({ pathname }: { pathname: string }) {
     {
       icon: Bell,
       label: '알림',
-      onClick: () => {
-        //   console.log('알림 클릭');
-      },
+      onClick: () => {},
     },
     {
       icon: User,
       label: '마이 페이지',
       className: `fill-white w-[27px] h-[27px] ${pathname === '/mypage' ? 'fill-tertiary hover:fill-tertiary' : ''}`,
       onClick: (router: RouterType) => {
-        //   console.log('유저 클릭');
         router.push('/mypage');
       },
     },
@@ -32,7 +29,6 @@ function HeaderBtn({ pathname }: { pathname: string }) {
       label: '로그인',
       className: 'sm:block hidden',
       onClick: () => {
-        //   console.log('로그아웃 클릭');
         router.push('/login');
       },
     },
