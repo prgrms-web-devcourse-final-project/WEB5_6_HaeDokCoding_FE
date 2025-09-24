3fb27319910ee96d239e73779e4f1657f9bd7d39
mtm1018 <mtm1018@naver.com>
2025-09-24 11:02:42 +0900

    [style]keep컴포넌트 아이콘 편집

--- src/shared/components/keep/Keep.tsx
+++ src/shared/components/keep/Keep.tsx
@@ -1,5 +1,6 @@
 import KeepIcon from '@/shared/assets/icons/keep_36.svg';
 import KeepIconActive from '@/shared/assets/icons/keep_active_36.svg';
+
 import { useState } from 'react';
 function Keep() {
   const [isClick, setIsClick] = useState(false);
@@ -10,11 +11,22 @@ function Keep() {
   return (
     <button type="button" onClick={handleClick} aria-label="킵 버튼">
       {isClick ? (
-        <KeepIcon fill="transparent" className="[&_*]:duration-100 hover:[&_*]:fill-secondary/50" />
+        <KeepIcon
+          fill="transparent"
+          className="[filter:drop-shadow(0_0_8px_rgba(255,255,255,0.8))] [&_*]:duration-100 hover:[&_*]:fill-secondary/60"
+          aria-hidden
+        />
       ) : (
         <KeepIconActive
           fill="transparent"
-          className="[&_*]:duration-100 hover:[&_*]:fill-secondary/50 "
+          className="   
+          [filter:drop-shadow(0_0_8px_rgba(255,255,255,0.8))]
+         [&_*]:duration-200
+         &_g>g>path[stroke]]:stroke-black
+          [&_g>path[fill='black']]:black
+          hover:[&_g>g>path]:fill-secondary/60
+          "
+          aria-hidden
         />
       )}
     </button>
