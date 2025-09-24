74687b66eefb03b995ae2146d22022c2ad9e26f5
mtm1018 <mtm1018@naver.com>
2025-09-24 11:28:58 +0900

    [style]레시피 페이지 제작

--- src/app/recipe/page.tsx
+++ src/app/recipe/page.tsx
@@ -1,4 +1,10 @@
+import Image from 'next/image'
+import Star from '@/shared/assets/images/star_2.webp'
 function Page() {
-  return <div>recipe</div>;
+  return (
+    <div className="page-layout max-w-1224">
+      <Image src={Star}></Image>
+    </div>
+  )
 }
 export default Page;
