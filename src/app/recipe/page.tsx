0a7e7764b15e320415b0521277ab61be57fb2d1b
mtm1018 <mtm1018@naver.com>
2025-09-24 15:47:12 +0900

    [style]레시피페이지

--- src/app/recipe/page.tsx
+++ src/app/recipe/page.tsx
@@ -1,13 +1,33 @@
 import PageHeader from '@/shared/components/pageHeader/PageHeader';
 import { Metadata } from 'next';
 import Glass from '@/shared/assets/images/recipe_page_header.png';
+import SelectBox from '@/shared/components/InputBox/SelectBox';
+import Input from '@/shared/components/InputBox/Input';
+
+import Cocktails from '@/shared/components/recipePage/cocktails/Cocktails';
 
 export const metadata: Metadata = {
   title: 'SSOUL | 칵테일레시피',
   description: '칵테일 레시피가 궁금하신 분들을 위한 레시피 페이지',
 };
 
+export const selectOption = [
+  {
+    option: ['', '약한 도수', '가벼운 도수', '중간 도수', '센 도수', '매우 센 도수'],
+    title:'도수'
+  },
+  {
+    option: ['', '위스키', '진', '럼', '보드카', '데킬라', '리큐르'],
+    title:'베이스'
+  }, {
+    option: ['', '클래식', '롱', '슈터', '숏'],
+    title:'글라스'
+  }
+]
 function Page() {
+
+
+
   return (
     <div className="w-full">
       <PageHeader
@@ -15,7 +35,25 @@ function Page() {
         title="Cocktail Recipes"
         description="다양하고 재밌는 칵테일 레시피"
       />
-      <div className="page-layout max-w-1224"></div>
+      <div className="page-layout max-w-1224 mt-3">
+        <div className="flex justify-between items-center">
+          <ul className="flex gap-3">
+            {selectOption.map(({ option, title }, i) => (
+              <li key={title + i}>
+                <SelectBox option={option} title={title} />
+              </li>
+            ))}
+          </ul>
+          <Input placeholder="내용을 입력해 주세요." id="search" variant="search" />
+        </div>
+        <div className='flex justify-between items-center mt-3'>
+          <p>n개</p>
+          <SelectBox option={['','댓글순']} title='최신순' />
+        </div>
+        <ul>
+          <Cocktails/>
+        </ul>
+      </div>
     </div>
   );
 }
