390a667951eb7fc8c5a51dec06fbfece2da15a46
ahk0413 <rladkgusgd@gmail.com>
2025-09-25 11:56:27 +0900

    [feat] 레이아웃 footer 로직 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import '@/shared/styles/global.css';
 import { Toaster } from 'react-hot-toast';
 import ScrollTopBtn from '@/shared/components/scrollTop/ScrollTopBtn';
 import Header from '@/shared/components/header/Header';
-import Footer from '@/shared/components/footer/Footer';
+import FooterWrapper from '@/shared/components/footer/FooterWrapper';
 export const metadata: Metadata = {
   title: 'SSOUL',
   description: '칵테일을 좋아하는 사람들을 위한 서비스',
@@ -22,7 +22,7 @@ export default function RootLayout({
           <div id="observer-target" className="h-[0.5px]"></div>
           {children}
         </main>
-        <Footer />
+        <FooterWrapper />
 
         <div id="modal-root"></div>
         <Toaster
