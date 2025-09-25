711895230dc66b216c188280344c68b967cc9a3d
ahk0413 <rladkgusgd@gmail.com>
2025-09-25 11:57:17 +0900

    [style] 취향 추천페이지 작업

--- src/app/recommend/page.tsx
+++ src/app/recommend/page.tsx
@@ -1,5 +1,7 @@
 import Bg from '@/shared/assets/images/recommend_bg.webp';
-import ChatForm from './ChatForm';
+import ChatForm from './components/ChatForm';
+import SsuryChat from './components/SsuryChat';
+import MyChat from './components/MyChat';
 
 function Page() {
   return (
@@ -7,7 +9,13 @@ function Page() {
       className="bg-repeat-y bg-top bg-auto w-full flex"
       style={{ backgroundImage: `url(${Bg.src})` }}
     >
+      <h1 className="sr-only">취향추천하기</h1>
       <div className="relative page-layout max-w-1024 py-12 ">
+        <div className="flex flex-col gap-10 pb-20">
+          <SsuryChat />
+          <MyChat></MyChat>
+        </div>
+
         <ChatForm />
       </div>
     </div>
