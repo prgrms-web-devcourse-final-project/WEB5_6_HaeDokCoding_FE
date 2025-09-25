67bf319122038e5dd978274451f6dbcf50305410
EunbinJung <ainatube39@gmail.com>
2025-09-25 11:14:26 +0900

    [style] 수정

--- src/shared/components/community/detail/EditDelete.tsx
+++ src/shared/components/community/detail/EditDelete.tsx
@@ -3,7 +3,7 @@ function EditDelete() {
     <div
       role="group"
       aria-label="게시글 수정 및 삭제"
-      className="sm:gap-3 gap-1 sm:text-[14px] text-[12px]"
+      className="flex sm:gap-3 gap-1 sm:text-[14px] text-[12px] text-gray"
     >
       <button type="button" className="">
         수정
