4bba1c89ecb3c4f0b851fdc0edf5e71d1764d9d1
mtm1018 <mtm1018@naver.com>
2025-09-25 10:22:09 +0900

    [feat]selectBox컴포넌트 다양화

--- src/shared/components/InputBox/SelectBox.tsx
+++ src/shared/components/InputBox/SelectBox.tsx
@@ -1,24 +1,53 @@
 'use client';
-import { Ref, useState } from 'react';
+import { Ref, useMemo, useState } from 'react';
 import Down from '@/shared/assets/icons/selectDown_24.svg';
+import { ID, useAccordionStore } from '@/shared/@store/store';
+import { useShallow } from 'zustand/shallow';
 
 interface Props {
+  id?: ID;
+  groupKey?: string;
   ref?: Ref<HTMLButtonElement | null>;
   option: string[];
   title: string;
+  onChange?: (value: string) => void;
 }
 
-function SelectBox({ ref, option, title }: Props) {
+// groupKey를 Props로 내릴경우 == 아코디언 없는 경우 == select박스
+function SelectBox({ id, groupKey, ref, option, title, onChange }: Props) {
   const [isOpen, setIsOpen] = useState(false);
   const [select, setSelect] = useState('');
 
+  const ingroup = !!groupKey;
+  // groupkey일 경우 전달받은 ID로 식별 아닐경우 title로 식별
+  const keyId = useMemo<ID>(() => id ?? title, [id, title]);
+
+  const { openId, toggleGroup, closeGroup } = useAccordionStore(
+    useShallow((s) => ({
+      openId: ingroup ? (s.openByGroup[groupKey] ?? null) : null,
+      toggleGroup: s.toggle,
+      closeGroup: s.closeGroup,
+    }))
+  );
+
+  //groupkey가 있을 떄와 없을때로 구분해서 state혹은 store로 관리
+  const localOpen = ingroup ? openId === keyId : isOpen;
+
+  const toggle = () => {
+    if (ingroup) toggleGroup(groupKey, keyId);
+    else setIsOpen((v) => !v);
+  };
+
+  const close = () => {
+    if (ingroup) closeGroup(groupKey);
+    else setIsOpen(false);
+  };
+
   const handleChoose = (v: string) => {
-    setIsOpen(!isOpen);
-    if (!v) {
-      setSelect(title);
-    } else {
-      setSelect(v);
-    }
+    const value = v || title;
+    setSelect(value);
+    onChange?.(value);
+    close();
   };
 
   return (
@@ -26,11 +55,11 @@ function SelectBox({ ref, option, title }: Props) {
       <button
         ref={ref}
         className="flex gap-2 cursor-pointer text-base"
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={toggle}
         aria-expanded={isOpen}
       >
         {select ? select : title}
-        {isOpen ? (
+        {localOpen ? (
           <Down className="rotate-180 duration-300" />
         ) : (
           <Down className="rotate-0 duration-300" />
@@ -38,8 +67,8 @@ function SelectBox({ ref, option, title }: Props) {
       </button>
 
       <ul
-        className={`w-30 bg-white text-gray-dark p-2 rounded-xl  duration-200  absolute transition-all 
-         ${isOpen ? 'opacity-100 top-8 right-0' : 'opacity-0 pointer-events-none top-4 right-0'}`}
+        className={`w-30 bg-white text-gray-dark p-2 rounded-xl z-99 duration-200  absolute transition-all 
+         ${localOpen ? 'opacity-100 top-8 right-0' : 'opacity-0 pointer-events-none top-4 right-0'}`}
         role="listbox"
       >
         {option.map((v, i) => (
