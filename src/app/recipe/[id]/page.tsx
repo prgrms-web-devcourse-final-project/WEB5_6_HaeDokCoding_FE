06d7dd748f23406d2ae16fe71595224938ba5675
mtm1018 <mtm1018@naver.com>
2025-09-25 11:08:36 +0900

    [feat] StarBg컴포넌트

---
+++ src/app/recipe/[id]/page.tsx
@@ -0,0 +1,10 @@
+import StarBg from '@/shared/components/starBg/StarBg';
+
+function page() {
+  return (
+    <div className="w-full">
+      <StarBg className="h-200 lg:h-202" />
+    </div>
+  );
+}
+export default page;
