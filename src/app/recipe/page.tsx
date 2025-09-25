3bd220059c488ce6d360125efcd7f4726bf4926f
mtm1018 <mtm1018@naver.com>
2025-09-25 10:22:09 +0900

    [feat]selectBox컴포넌트 다양화

--- src/app/recipe/page.tsx
+++ src/app/recipe/page.tsx
@@ -4,6 +4,7 @@ import Glass from '@/shared/assets/images/recipe_page_header.webp';
 import SelectBox from '@/shared/components/InputBox/SelectBox';
 import Input from '@/shared/components/InputBox/Input';
 import CocktailList from '@/shared/components/recipePage/cocktailList/CocktailList';
+import Accordion from './Accordion';
 
 export const metadata: Metadata = {
   title: 'SSOUL | 칵테일레시피',
@@ -12,37 +13,19 @@ export const metadata: Metadata = {
 
 // select는 좌측정렬 검색창 꽉차게
 
-const selectOption = [
-  {
-    option: ['', '약한 도수', '가벼운 도수', '중간 도수', '센 도수', '매우 센 도수'],
-    title: '도수',
-  },
-  {
-    option: ['', '위스키', '진', '럼', '보드카', '데킬라', '리큐르'],
-    title: '베이스',
-  },
-  {
-    option: ['', '클래식', '롱', '슈터', '숏'],
-    title: '글라스',
-  },
-];
 function Page() {
   return (
     <div className="w-full">
-      <PageHeader
-        src={Glass}
-        title="Cocktail Recipes"
-        description="다양하고 재밌는 칵테일 레시피"
-      />
+      <section>
+        <PageHeader
+          src={Glass}
+          title="Cocktail Recipes"
+          description="다양하고 재밌는 칵테일 레시피"
+        />
+      </section>
       <div className="page-layout max-w-1224 mt-6">
         <section className="flex flex-col-reverse items-center gap-6 md:flex-row md:justify-between md:items-center ">
-          <ul className="flex gap-3">
-            {selectOption.map(({ option, title }, i) => (
-              <li key={title + i}>
-                <SelectBox option={option} title={title} />
-              </li>
-            ))}
-          </ul>
+          <Accordion />
           <Input
             placeholder="내용을 입력해 주세요."
             id="search"
--- src/shared/components/pageHeader/PageHeader.tsx
+++ src/shared/components/pageHeader/PageHeader.tsx
@@ -18,9 +18,9 @@ function PageHeader({ src, title, description }: Props) {
       <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
         <Image src={src} alt="" width={src.width} height={src.height} />
         <div className="text-center text-nowrap font-serif font-bold flex flex-col absolute left-1/2 -translate-x-1/2 bottom-4 md:bottom-9 lg:bottom-[55px]">
-          <h2 className="text-shadow-[0_0_12px_#1a1a1a]  text-3xl md:text-5xl lg:text-3xl lg:text-[64px]">
+          <h1 className="text-shadow-[0_0_12px_#1a1a1a]  text-3xl md:text-5xl lg:text-3xl lg:text-[64px]">
             {title}
-          </h2>
+          </h1>
           <p className="text-shadow-[0_0_12px_#1a1a1a] text-lg md:text-xl lg:text-2xl">
             {description}
           </p>
