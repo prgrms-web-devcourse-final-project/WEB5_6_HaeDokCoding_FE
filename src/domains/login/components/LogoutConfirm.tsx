26b9e365f78e15dae438dbba15aa1c152e866487
ahk0413 <rladkgusgd@gmail.com>
2025-09-26 11:10:29 +0900

    [feat] 로그아웃 팝업 추가

--- src/domains/login/components/LogoutConfirm.tsx
+++ src/domains/login/components/LogoutConfirm.tsx
@@ -1,4 +1,11 @@
-function LoginConfirm() {
-  return <div>LoginConfirm</div>;
+import ConfirmModal from '@/shared/components/modalPop/ConfirmModal';
+
+interface Props {
+  open: boolean;
+  onClose: () => void;
+}
+
+function LoginConfirm({ open, onClose }: Props) {
+  return <ConfirmModal open={open} onClose={onClose} description="정말 로그아웃 하시겠어요?" />;
 }
 export default LoginConfirm;
