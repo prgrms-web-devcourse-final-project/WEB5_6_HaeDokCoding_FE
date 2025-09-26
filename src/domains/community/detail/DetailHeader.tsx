78e3ecddc5f071905b0090a1226e1a98bb55bacb
mtm1018 <mtm1018@naver.com>
2025-09-26 10:30:58 +0900

    [fix]경로수정

--- src/domains/community/detail/DetailHeader.tsx
+++ src/domains/community/detail/DetailHeader.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import Label from '@/domains/shared/label/Label';
-=======
-import Label from '../../label/Label';
->>>>>>> 5754d67 ([docs] 폴더정리)
 import EditDelete from './EditDelete';
 
 function DetailHeader() {
