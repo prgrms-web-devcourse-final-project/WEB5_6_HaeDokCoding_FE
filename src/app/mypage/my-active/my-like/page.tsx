9125eefa730bebab793b884bbc690c41052f0c76
EunbinJung <ainatube39@gmail.com>
2025-09-30 17:53:29 +0900

    주석처리

--- src/app/mypage/my-active/my-like/page.tsx
+++ src/app/mypage/my-active/my-like/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { fetchPost } from '@/domains/community/api/fetchPost';
 import PostCard from '@/domains/community/main/PostCard';
 import { Metadata } from 'next';
 
@@ -8,28 +7,8 @@ export const metadata: Metadata = {
   title: 'SSOUL | 마이페이지',
   description: 'SSOUL 서비스에서 나의 활동을 관리할 수 있는 페이지입니다',
 };
-import { Post } from '@/domains/community/types/post';
-import { useEffect, useState } from 'react';
 
 function Page() {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      const data = await fetchPost();
-      if (!data) return;
-      setPosts(data);
-      setIsLoading(false);
-    };
-    fetchData();
-  }, [setPosts]);
-
-  return (
-    <section>
-      <PostCard posts={posts} isLoading={isLoading} />
-    </section>
-  );
+  return <section>{/* <PostCard posts={posts} isLoading={isLoading} /> */}</section>;
 }
 export default Page;
