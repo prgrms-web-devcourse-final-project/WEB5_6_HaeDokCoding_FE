27c5a271010d04b645373b56a3ee25d717685ad6
EunbinJ <ainatube39@gmail.com>
2025-10-10 11:42:46 +0900

    Feat/communityscroll#23 (#114)
    * [feat] 스크롤링구현
    
    * [feat] 주소, api설정
    
    * [feat] 커뮤니티 탭, 필터 패치로직
    
    * [feat] lastLikeCount, lastCommentCount, 추가
    
    * [fix] 코멘트 삭제수정 마이페이지에선 뗄수있게 myPage props 추가
    
    * 옵셔널로 수정
    
    * 오류 수정

--- src/domains/community/main/Community.tsx
+++ src/domains/community/main/Community.tsx
@@ -1,27 +1,100 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import CommunityFilter from './CommunityFilter';
 import CommunityTab from './CommunityTab';
 import PostCard from './PostCard';
 import WriteBtn from './WriteBtn';
 import { Post } from '../types/post';
-import { fetchPost } from '../api/fetchPost';
+import { fetchPostByTab } from '../api/fetchPost';
+import { useSearchParams } from 'next/navigation';
 
 function Community() {
-  const [posts, setPosts] = useState<Post[]>([]);
+  const [posts, setPosts] = useState<Post[] | null>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [lastLoadedId, setLastLoadedId] = useState<number | null>(null);
+
+  const searchParams = useSearchParams();
+
+  const category = useMemo(() => searchParams.get('category') || 'all', [searchParams]);
+  const filter = useMemo(
+    () => (searchParams.get('postSortStatus') as 'LATEST' | 'POPULAR' | 'COMMENTS') || 'LATEST',
+    [searchParams]
+  );
+
+  const [isEnd, setIsEnd] = useState(false);
 
   useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      const data = await fetchPost();
-      if (!data) return;
-      setPosts(data);
+    setPosts([]);
+    setIsEnd(false);
+    setLastLoadedId(null);
+    loadInitialPosts();
+  }, [category, filter]);
+
+  const loadInitialPosts = async () => {
+    const category = searchParams.get('category') || 'all';
+    const filter =
+      (searchParams.get('postSortStatus') as 'LATEST' | 'POPULAR' | 'COMMENTS') || 'LATEST';
+
+    setIsLoading(true);
+    setIsEnd(false);
+
+    const lastLikeCount =
+      posts && posts.length > 0 ? Math.min(...posts.map((post) => post.likeCount)) : null;
+
+    const lastCommentCount =
+      posts && posts.length > 0 ? Math.min(...posts.map((post) => post.commentCount)) : null;
+
+    try {
+      const newPosts = await fetchPostByTab({
+        category,
+        filter,
+        lastLikeCount,
+        lastCommentCount,
+      });
+
+      if (!newPosts || newPosts.length === 0) {
+        setIsEnd(true);
+        setPosts([]);
+      } else {
+        setPosts(newPosts);
+      }
+    } finally {
       setIsLoading(false);
-    };
-    fetchData();
-  }, [setPosts]);
+    }
+  };
+
+  const loadMorePosts = async (lastPostId: number) => {
+    if (isEnd || isLoading) return;
+    if (!posts || posts.length === 0) return;
+    console.log('시작', lastPostId);
+
+    const lastPost = posts[posts.length - 1];
+    if (lastPostId === lastPost.postId) return;
+    setLastLoadedId(lastPost.postId);
+
+    setIsLoading(true);
+    try {
+      const category = searchParams.get('category') || 'all';
+      const filter =
+        (searchParams.get('postSortStatus') as 'LATEST' | 'POPULAR' | 'COMMENTS') || 'LATEST';
+
+      const newPosts = await fetchPostByTab({
+        category,
+        filter,
+        lastId: lastPostId,
+      });
+
+      if (!newPosts || newPosts?.length === 0) {
+        setIsEnd(true);
+        console.log('끝');
+      } else {
+        setPosts((prev) => [...(prev ?? []), ...(newPosts ?? [])]);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
@@ -29,13 +102,20 @@ function Community() {
         aria-label="탭과 글쓰기"
         className="flex justify-between item-center sm:flex-row flex-col gap-4 mt-1"
       >
-        <CommunityTab setPosts={setPosts} />
+        <CommunityTab setPosts={setPosts} setIsLoading={setIsLoading} setIsEnd={setIsEnd} />
         <WriteBtn />
       </section>
 
       <section aria-label="게시물 목록">
-        <CommunityFilter posts={posts} />
-        <PostCard posts={posts} isLoading={isLoading} />
+        <CommunityFilter posts={posts} setPosts={setPosts} />
+        <PostCard
+          posts={posts}
+          setPost={setPosts}
+          isLoading={isLoading}
+          setIsLoading={setIsLoading}
+          isEnd={isEnd}
+          onLoadMore={loadMorePosts}
+        />
       </section>
     </>
   );
