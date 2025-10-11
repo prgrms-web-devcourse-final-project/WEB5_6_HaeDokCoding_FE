0df387643135d90f8463097ff7040883dc10271a
EunbinJung <ainatube39@gmail.com>
2025-10-11 12:47:44 +0900

    [feat] 프로필 쑤리 이미지

--- src/domains/shared/components/profile/Profile.tsx
+++ src/domains/shared/components/profile/Profile.tsx
@@ -1,11 +1,30 @@
+import useFetchProfile from '@/domains/mypage/api/fetchProfile';
+import SsuryImage from '@/domains/mypage/main/SsuryImage';
+import { useEffect } from 'react';
+
 type Props = {
   userNickname: string;
 };
 
 function Profile({ userNickname }: Props) {
+  const { profile, fetchProfile } = useFetchProfile();
+  useEffect(() => {
+    fetchProfile();
+  }, [profile?.abvLevel]);
+
+  if (!profile) return;
+  const { abvLevel } = profile;
+
   return (
     <div className="flex gap-2 items-center justify-start">
-      <div className="w-7 h-7 rounded-full bg-secondary" aria-label="작성자 아이콘"></div>
+      <div
+        className="w-9 h-9 rounded-full bg-secondary flex items-center justify-center"
+        aria-label="작성자 아이콘"
+      >
+        <div className="w-8 flex items-center justify-center">
+          <SsuryImage abvLevel={abvLevel} />
+        </div>
+      </div>
       <span aria-label="작성자 이름" className="text-sm">
         {userNickname}
       </span>
