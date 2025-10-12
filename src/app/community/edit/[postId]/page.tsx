f5a387030fcde40a2dca34ac4f79527e5c00d680
EunbinJung <ainatube39@gmail.com>
2025-10-12 17:14:03 +0900

    타입수정

--- src/app/community/edit/[postId]/page.tsx
+++ src/app/community/edit/[postId]/page.tsx
@@ -1,22 +1,18 @@
 'use client';
 
-import TagModal from '@/domains/community/write/cocktail-tag/TagModal';
 import WriteSection from '@/domains/community/write/WriteSection';
 import StarBg from '@/domains/shared/components/star-bg/StarBg';
 import { useParams } from 'next/navigation';
-import { useState } from 'react';
 
 function Page() {
-  const [isOpen, setIsOpen] = useState(false);
   const params = useParams();
 
   return (
     <div className="w-full mb-20 flex relative">
       <StarBg className="w-full h-32 absolute"></StarBg>
       <div className="page-layout max-w-824 flex-1 z-5">
-        <WriteSection setIsOpen={setIsOpen} mode="edit" postId={params.postId} />
+        <WriteSection mode="edit" postId={params.postId} />
       </div>
-      {isOpen && <TagModal isOpen={isOpen} setIsOpen={setIsOpen} />}
     </div>
   );
 }
